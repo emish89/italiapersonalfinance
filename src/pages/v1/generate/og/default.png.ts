3681fe6598bd2822ca5600e3921ae5ceab332c61
fballarini <emish89@yahoo.it>
2023-11-09 18:15:05 +0100

    chore(deploy): first attempt to deploy to gh pages

--- src/pages/v1/generate/og/default.png.ts
+++ src/pages/v1/generate/og/default.png.ts
@@ -13,19 +13,19 @@ const height = 630;
 const width = 1200;
 
 export const GET: APIRoute = async () => {
-  const link = "TODO";
+  const link = "https://brutal.elian.codes"; //TODO
   const html = toReactElement(`
   <div style="background-color: white; display: flex; flex-direction: column; height: 100%; padding: 3rem; width: 100%">
     <div style="display:flex; height: 100%; width: 100%; background-color: white; border: 6px solid black; border-radius: 0.5rem; padding: 2rem; filter: drop-shadow(6px 6px 0 rgb(0 0 0 / 1));">
       <div style="display: flex; flex-direction: column; justify-content: space-between; width: 100%; filter: drop-shadow()">
         <div style="display: flex; flex-direction: column; gap: 0.75rem;">  
-          <p style="font-size: 48px;">IPF</p>
-          <p style="font-size: 38px;">TODO</p>
-          <p style="font-size: 38px;">TODO</p>
+          <p style="font-size: 48px;">Brutal</p>
+          <p style="font-size: 38px;">A theme for Astro</p>
+          <p style="font-size: 38px;">Brought to you by Elian</p>
         </div>
         <div style="display: flex; justify-content: space-between; align-items: baseline; padding-top: -2rem;">
           <p style="font-size: 32px">${link}</p>
-          <img src="TODO" width="200px" height="200px" style="border: 3px solid black; border-radius: 0.5rem;" />
+          <img src="https://www.elian.codes/assets/img/elian.jpg" width="200px" height="200px" style="border: 3px solid black; border-radius: 0.5rem;" />
         </div>
       </div>
     </div>
