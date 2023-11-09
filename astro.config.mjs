5e811f9767eb42948d9657579e84471557e92f4c
Federico Ballarini <emish89@yahoo.it>
2023-11-09 17:49:59 +0100

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import UnoCSS from 'unocss/astro';
+
+export default defineConfig({
+  // used to generate images
+  site:
+    process.env.VERCEL_ENV === 'production'
+      ? 'https://brutal.elian.codes/'
+      : process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/`
+      : 'https://localhost:3000/',
+  trailingSlash: 'ignore',
+  integrations: [sitemap(), UnoCSS({ injectReset: true })],
+  experimental: {
+    devOverlay: true,
+  },
+  vite: {
+    optimizeDeps: {
+      exclude: ['@resvg/resvg-js'],
+    },
+  },
+});
