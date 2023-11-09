12629893b792f93b7339abd0e7f91cb9f1d7a26d
Federico Ballarini <federico.ballarini.sc@gmail.com>
2023-11-09 21:59:48 +0100

    chore(deps): readded package-lock

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import UnoCSS from 'unocss/astro';
 
 export default defineConfig({
   // used to generate images
-  site: 'https://emish89.github.io',
+  site: 'https://italia-personal-finance.netlify.app/',
   trailingSlash: 'ignore',
   integrations: [sitemap(), UnoCSS({ injectReset: true })],
   experimental: {
