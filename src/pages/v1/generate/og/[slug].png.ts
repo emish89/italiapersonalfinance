971b44235fe745542868625a8beeb0d2bed01818
Federico Ballarini <federico.ballarini.sc@gmail.com>
2023-11-20 23:32:51 +0100

    feat(og): og image implemented

--- src/pages/v1/generate/og/[slug].png.ts
+++ src/pages/v1/generate/og/[slug].png.ts
@@ -33,7 +33,7 @@ export const GET: APIRoute = async ({ params, props }) => {
             <p style="font-size: 48px;">IPF</p>
             <p style="font-size: 38px;">${title}</p>
           </div>
-          <img src="https://www.elian.codes/assets/img/elian.jpg" width="200px" height="200px" style="border: 3px solid black; border-radius: 0.5rem;" />
+          <img src="https://italia-personal-finance.netlify.app/ipf.jpg" width="200px" height="200px" style="border: 3px solid black; border-radius: 0.5rem;" />
         </div>
         <div style="display: flex;">
           <p style="font-size: 24px;">${description}</p>
