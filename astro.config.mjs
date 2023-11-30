7316ecb4b9915d07797dd13ce7dbdfb6b3fdd8e6
fballarini <emish89@yahoo.it>
2023-11-30 13:18:25 +0100

    fix(partytown): removed partytown because of firefox bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import UnoCSS from 'unocss/astro';
 import mdx from "@astrojs/mdx";
-import partytown from "@astrojs/partytown";
 import { remarkModifiedTime } from './remark-modified-time.mjs';
 import { remarkReadingTime } from './remark-reading-time.mjs';
 
@@ -16,12 +15,15 @@ export default defineConfig({
   integrations: [sitemap(), UnoCSS({
     injectReset: true
   }), mdx(), react(),
-  partytown({
-    // Adds dataLayer.push as a forwarding-event.
-    config: {
-      forward: ["dataLayer.push"],
-    },
-  }),
+    /*
+    TODO waiting for firefox bug resolution
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    */
   ],
   experimental: {
     devOverlay: true
