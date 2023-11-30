d7111dc6460378fab540b3e532344e5e103582f3
Leonardo <leonardo.bianco@outlook.com>
2023-11-30 08:45:00 +0000

    Disabilitato momentaneamente astro partytown per bug noto con firefox

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import UnoCSS from 'unocss/astro';
 import mdx from "@astrojs/mdx";
-import partytown from "@astrojs/partytown";
+//import partytown from "@astrojs/partytown";
 import { remarkModifiedTime } from './remark-modified-time.mjs';
 import { remarkReadingTime } from './remark-reading-time.mjs';
 
@@ -16,12 +16,12 @@ export default defineConfig({
   integrations: [sitemap(), UnoCSS({
     injectReset: true
   }), mdx(), react(),
-  partytown({
+  /*partytown({
     // Adds dataLayer.push as a forwarding-event.
     config: {
       forward: ["dataLayer.push"],
     },
-  }),
+  }),*/
   ],
   experimental: {
     devOverlay: true
